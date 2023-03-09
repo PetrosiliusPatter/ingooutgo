dfc180975bf21018da34b66df29f16aed14d894b
PetrosiliusPatter <petrosiliuspatter@proton.me>
2023-03-09 22:24:48 +0100

    Updates

--- index.js
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./containers/App/App";
-const root = createRoot(document.getElementById("root") || document.createElement("div"));
-root.render(React.createElement(App, null));
