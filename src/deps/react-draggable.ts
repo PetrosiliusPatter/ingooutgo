7c1cba87a2c13bacaf4fd9cef7bc8d5255e89e35
PetrosiliusPatter <petrosiliuspatter@proton.me>
2023-12-30 13:52:07 +0100

    Fixed dependency of react-draggable

--- src/deps/react-draggable.ts
+++ src/deps/react-draggable.ts
@@ -1,2 +1,2 @@
-export type { DraggableEventHandler } from "npm:react-draggable@4.4.5"
-export { default as Draggable } from "npm:react-draggable@4.4.5"
+export type { DraggableEventHandler } from "https://esm.sh/react-draggable@4.4.5"
+export { default as Draggable } from "https://esm.sh/react-draggable@4.4.5"
